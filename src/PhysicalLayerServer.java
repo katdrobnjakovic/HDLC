b8ef64e6b3ab581f8dad50d9320074d6de5a53ec
Emilienne Pugin <mpugi055@uottawa.ca>
2014-02-27 11:12:20 -0500

    Cleaned up provided lab template & added .gitignore
    Cleaned up by:
    Editing comments, variable names, some slight structure changes
    Gitignore contains the .metadata, bin, and .project folders

--- src/PhysicalLayerServer.java
+++ src/PhysicalLayerServer.java
@@ -1,59 +1,194 @@
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.InterruptedIOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 class PhysicalLayerServer 
 {
-	
+
+
 	public static void main(String args[])
 	{
-		ServerSocket server = null; 
-		Socket clientSocket = null; 
-		DataInputStream input = null;
-		DataOutputStream output = null;
 		
-		try 
-		{
-			server = new ServerSocket(4444);
-			System.out.println("Server running on port 4444");
+		// sockets and other variables declaration
+		// maximum number of clients connected: 10                    ASSUMPTION
 
-			//accepts the socket connection
-			clientSocket = server.accept();
-			
-			//while the socket is open allow an input and output stream
-			while (true) {
-				input = new DataInputStream(clientSocket.getInputStream());
-				output = new DataOutputStream(clientSocket.getOutputStream());
-			}
-			
-		} catch (IOException e)
-		{
-			// print stack trace if the above try throws an input output exception
-			e.printStackTrace();
-		}
+		ServerSocket serverSocket = null;
+		Socket[] clientSockets;
+		clientSockets = new Socket[10];
+		PrintWriter[] printWriter;
+		printWriter = new PrintWriter[10];
+		BufferedReader[] bufferedReader;
+		bufferedReader = new BufferedReader[10];
+
+		int[] ns; // send sequence number
+		ns = new int[10];
+
+		int[] nr; // receive sequence number
+		nr = new int[10];
+
+		String inputLine = null;
+		String outputLine = null;
+
+		//get port number from the command line
+		int nPort = 4444; // default port number        
+
+		String flag = "01111110";
+		String[] address;
+		address = new String[10];
+		int[] clientID;
+		clientID = new int[10];
+
+		String control = null;
+		String information = "";
+
+		boolean bListening = true;
+
+		String[] sMessages; // frame buffer
+		sMessages = new String[20];
+		int nMsg = 0;        
+
 		
-		try
-		{
-			//cleanup - close the input and output streams and the server connection
-			input.close();
-			output.close();
-			clientSocket.close();
-			server.close();
-		} catch (IOException e) {
-			//print stack trace if the above try throws an input output exception
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * This method handles message from the secondary stations. 
-	 * @param msg, the message received from the secondary station. 
-	 */
-	public static void handleMessageFromSecondaryStation(String msg) 
-	{
+		boolean bAlive = false;
+
+		String response = null; // control field of the input
 		
+		// initialize some var's to handle the array of clients
+		int clientCount = 0;
+		int i = 0;       
+
+		// create server socket
+		try {
+			serverSocket = new ServerSocket(nPort);
+
+		// this variable defines how many clients are connected
+		int nClient = 0;
+
+		// set timeout on the socket so the program does not hang up
+			serverSocket.setSoTimeout(1000);
+
+			// main server loop
+			while (bListening){
+
+				try {  
+					
+					// trying to listen to the socket to accept clients
+					// if there is nobody to connect, exception will be thrown - set by setSoTimeout()
+					clientSockets[clientCount]=serverSocket.accept();
+
+					// connection got accepted
+					if (clientSockets[clientCount]!=null){
+
+						System.out.println("Connection from " + clientSockets[clientCount].getInetAddress() + " accepted.");
+
+						System.out.println("accepted client");
+						printWriter[clientCount] = new PrintWriter(clientSockets[clientCount].getOutputStream(),true);
+						bufferedReader[clientCount] = new BufferedReader(new InputStreamReader(clientSockets[clientCount].getInputStream()));
+
+						clientID[clientCount] = clientCount+1;		
+
+						address[clientCount] = "00000000"+Integer.toBinaryString(clientID[clientCount]);
+						int len = address[clientCount].length();					
+						address[clientCount] = address[clientCount].substring(len-8);				
+
+						System.out.println("client address: " + address[clientCount]);
+
+						// send client address to the new client
+						printWriter[clientCount].println(address[clientCount]);
+
+
+						// ===========================================================
+						// insert codes here to send SNRM message
+						//        			        			
+						System.out.println("Sent SNRM to station " + clientID[clientCount]);            		
+						// ===============================================================
+
+						// Receive UA message
+						inputLine = bufferedReader[clientCount].readLine();
+						response = inputLine.substring(16, 24);
+
+						if(response.equals("11000110") || response.equals("11001110")) {
+							System.out.println("Received UA from station " + clientID[clientCount]);
+						}
+						else {
+							System.out.println("UA error -- station " + clientID[clientCount]);
+						}       
+
+						// initialize ns and nr
+						ns[clientCount] = -1;
+						nr[clientCount] = 0;
+
+						// increment count of clients
+						clientCount++;
+						nClient = clientCount;
+						bAlive = true;
+					}
+				}
+				catch (Exception e) {
+
+				}
+
+				for (i=0;i<clientCount;i++) {
+
+					// ==============================================================
+					// insert codes here to send “RR,*,P” msg     		
+
+					System.out.println("Sent < RR,*,P > to station " + clientID[i]);
+					// ==============================================================
+
+
+					// recv response from the client
+					inputLine = bufferedReader[i].readLine();
+
+					if(inputLine != null) {		
+
+						// get control field of the response frame
+						response = inputLine.substring(16, 24);
+
+						if(response.substring(0,4).equals("1000")) {
+							// recv “RR,*,F”, no data to send from B
+							System.out.println("Receive RR, *, F from station " + clientID[i]);
+						}
+						else if(response.substring(0, 1).equals("0")) {
+							// ==============================================================
+							// insert codes here to handle the frame “I, *, *” received
+
+
+							//if the frame is to the primary station; consume it        					
+
+							//if the frame is to the secondary station; buffer the frame to send
+
+
+							// ==============================================================
+						}
+					}
+				}
+
+				// ==============================================================
+				// insert codes here to send frames in the buffer       	
+
+				// send I frame
+
+				// ==============================================================
+
+				//
+				// stop server automatically when all clients disconnected:
+				if (!bAlive && clientCount > 0){// no active clients
+					System.out.println("All clients are disconnected - stopping");
+					bListening = false;
+				}
+
+			}// end of while loop
+		}   catch (Exception e1) {
+			e1.printStackTrace();
+		}
+
 	}
-	
+
 }
--- src/SecondaryStation.java
+++ src/SecondaryStation.java
@@ -3,61 +3,121 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 class SecondaryStation 
 {
 	public static void main(String args[])
 	{
-		//Declaration of variables for socket programming
-		Socket client = null;
-		DataInputStream input = null;
-		DataOutputStream output = null;
+		Socket clientSocket = null;
+		PrintStream printStream = null;
+		DataInputStream inputStream = null;
 
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+		String id = null;
+		String information = null;
+		String control = null;
+		String flag = "01111110";
+		String address = null;  
+		String response = null; // control field of the socket input
+		int ns = -1; // send sequence number
+		int nr = 0; //receive sequence number
+
+		String answer = null; // input using keyboard
+
+		//Initialization section:
+		//Try to open a socket on port 4444
+		//Try to open input and output streams
 		try {
-			//connect to server local host on port 4444
-			client = new Socket("localhost", 4444); 
-
-			//input and output streams for the socket
-			input = new DataInputStream(client.getInputStream());
-			output = new DataOutputStream(client.getOutputStream());
-
-			while(true)
-			{
-				//while the socket is open, read from the console
-				BufferedReader fromConsole = new BufferedReader(new InputStreamReader(System.in));
-				String message = fromConsole.readLine();
-
-				//send the message from the console to the server
-				PhysicalLayerServer.handleMessageFromSecondaryStation(message);
-
-
-				//cleanup - close the input and output streams and close the client connection
-				input.close();
-				output.close();
-				client.close();
-			}
-		} catch (IOException e) {
-			//prints stack trace if the above code throws an input output exception
-			e.printStackTrace();
+			clientSocket = new Socket("127.0.0.1", 4444);
+			printStream = new PrintStream(clientSocket.getOutputStream());
+			inputStream = new DataInputStream(clientSocket.getInputStream());
+		} 
+		catch (UnknownHostException e) {
+			System.err.println("Don't know about host: hostname");
+		} 
+		catch (IOException e) {
+			System.err.println("Couldn't get I/O for the connection to: hostname");
 		}
-	}
-	
-	/**
-	 * This method handles the message from the secondary stations. 
-	 * @param msg, the message received from the secondary station.
-	 */
-	public void handleMessageFromSecondayStation(String msg)
-	{
-		
-	}
-	
-	/**
-	 * This method handles the message from the physical layer server. 
-	 * @param msg, the message received from the physical layer server. 
-	 */
-	public void handleMessageFromPhysicalLayerServer(String msg)
-	{
-		
+
+		if (clientSocket != null && printStream != null && inputStream != null) {			
+
+			try {				
+
+				String responseLine;				
+				responseLine = inputStream.readLine();
+
+				//receive client address from the primary station
+				id = responseLine;
+				System.out.println("client address: " + id);
+
+				responseLine = inputStream.readLine();
+				response = responseLine.substring(16, 24);
+
+				// recv SNRM msg
+				if(response.equals("11000001") || response.equals("11001001")) {
+					//===========================================================
+					// insert codes here to send the UA msg					
+					//===========================================================
+					System.out.println("sent UA msg");
+				}
+
+				// main loop; recv and send data msgs
+				while (true) {
+					responseLine = inputStream.readLine();
+					response = responseLine.substring(16, 24);
+
+					System.out.println("recv msg -- control " + response);				
+
+					// recv ??RR,*,P?? msg
+					if(response.substring(0,5).equals("10001")) {
+
+						// enter data msg using keyboard 
+						System.out.println("Is there any message to send? (y/n)");
+						answer = bufferedReader.readLine();						
+
+						if(answer.toLowerCase().equals("y") || answer.toLowerCase().equals("yes")) {
+							System.out.println("Please enter the destination address using 8-bits binary string (e.g. 00000001):");
+							address = bufferedReader.readLine();
+
+							System.out.println("Please enter the message to send?");
+							answer = bufferedReader.readLine();
+
+							//===========================================================
+							// insert codes here to send an I msg;
+
+							//===========================================================
+
+						}				
+						else {
+							//===========================================================
+							// insert codes here to send ??RR,*,F??
+
+							//===========================================================
+						}
+					}
+
+					// receive an I frame
+					if(response.substring(0,1).equals("0")) {
+						String data = responseLine.substring(24, responseLine.length()-8);
+						System.out.println("");
+						System.out.println("Received data: " + data);						
+
+						nr = Integer.parseInt(response.substring(1,4), 2) + 1;
+						System.out.println("nr: " + nr);
+					}
+				}
+			} 
+			catch (UnknownHostException e) {
+				System.err.println("Trying to connect to unknown host: " + e);
+			} 
+			catch (IOException e) {
+				System.err.println("IOException: " + e);
+			}	
+		}
+
 	}
 }
