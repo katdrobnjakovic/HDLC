9740b7052f12ff25d51fad94cae04a318ca53f6b
Emilienne Pugin <emilienne.pugin@gmail.com>
2014-03-06 21:29:34 -0500

    Partially running
    TBD: SREJ & RR & ACKs

--- src/PhysicalLayerServer.java
+++ src/PhysicalLayerServer.java
@@ -11,21 +11,28 @@ import java.net.SocketException;
 
 class PhysicalLayerServer 
 {
-
-
+	static int clientCount = 0;
+	static int[] clientID = new int[10];
+	static DataInputStream[] inputStream = new DataInputStream[10];
+	static DataOutputStream[] outputStream = new DataOutputStream [10]; 
+	static String inputLine = null;
+	static String[] address = new String[10];
+	static String response = null; // control field of the input
+	static String control = null;
+	static PrintWriter[] printWriter = new PrintWriter[10];
+	
 	public static void main(String args[])
 	{
-		
+
 		// sockets and other variables declaration
 		// maximum number of clients connected: 10                    ASSUMPTION
 
 		ServerSocket serverSocket = null;
 		Socket[] clientSockets;
 		clientSockets = new Socket[10];
-		PrintWriter[] printWriter;
-		printWriter = new PrintWriter[10];
-		BufferedReader[] bufferedReader;
-		bufferedReader = new BufferedReader[10];
+	
+	
+
 
 		int[] ns; // send sequence number
 		ns = new int[10];
@@ -33,19 +40,17 @@ class PhysicalLayerServer
 		int[] nr; // receive sequence number
 		nr = new int[10];
 
-		String inputLine = null;
+	
 		String outputLine = null;
 
 		//get port number from the command line
-		int nPort = 4444; // default port number        
+		int nPort = 2732; // default port number        
 
 		String flag = "01111110";
-		String[] address;
-		address = new String[10];
-		int[] clientID;
-		clientID = new int[10];
 
-		String control = null;
+	
+
+		
 		String information = "";
 
 		boolean bListening = true;
@@ -54,42 +59,44 @@ class PhysicalLayerServer
 		sMessages = new String[20];
 		int nMsg = 0;        
 
-		
+
 		boolean bAlive = false;
 
-		String response = null; // control field of the input
-		
+
 		// initialize some var's to handle the array of clients
-		int clientCount = 0;
+	
 		int i = 0;       
 
 		// create server socket
 		try {
+			
 			serverSocket = new ServerSocket(nPort);
+			serverSocket.setSoTimeout(10000);
+			// this variable defines how many clients are connected
+			int nClient = 0;
 
-		// this variable defines how many clients are connected
-		int nClient = 0;
-
-		// set timeout on the socket so the program does not hang up
-			serverSocket.setSoTimeout(1000);
+			// set timeout on the socket so the program does not hang up
+			//serverSocket.setSoTimeout(1000);
 
 			// main server loop
 			while (bListening){
 
 				try {  
-					
+
 					// trying to listen to the socket to accept clients
 					// if there is nobody to connect, exception will be thrown - set by setSoTimeout()
 					clientSockets[clientCount]=serverSocket.accept();
-
+					System.out.println("Found Client");
 					// connection got accepted
 					if (clientSockets[clientCount]!=null){
 
 						System.out.println("Connection from " + clientSockets[clientCount].getInetAddress() + " accepted.");
 
 						System.out.println("accepted client");
-						printWriter[clientCount] = new PrintWriter(clientSockets[clientCount].getOutputStream(),true);
-						bufferedReader[clientCount] = new BufferedReader(new InputStreamReader(clientSockets[clientCount].getInputStream()));
+						//printWriter[clientCount] = new PrintWriter(clientSockets[clientCount].getOutputStream());
+						outputStream[clientCount] = new DataOutputStream(clientSockets[clientCount].getOutputStream());
+						inputStream = new DataInputStream [10];
+						inputStream[clientCount] = new DataInputStream(clientSockets[clientCount].getInputStream());
 
 						clientID[clientCount] = clientCount+1;		
 
@@ -100,25 +107,30 @@ class PhysicalLayerServer
 						System.out.println("client address: " + address[clientCount]);
 
 						// send client address to the new client
-						printWriter[clientCount].println(address[clientCount]);
+						outputStream[clientCount].writeUTF(address[clientCount]);
+						//printWriter[clientCount].println(address[clientCount]);
 
 
 						// ===========================================================
 						// insert codes here to send SNRM message
-						//        			        			
-						System.out.println("Sent SNRM to station " + clientID[clientCount]);            		
+						//   
+						outputStream[clientCount].writeUTF(flag + "00000000" + "11000001"+flag);
+			
+						System.out.println("Sent SNRM to station " + clientID[clientCount]);  
+					
 						// ===============================================================
 
 						// Receive UA message
-						inputLine = bufferedReader[clientCount].readLine();
-						response = inputLine.substring(16, 24);
+						//receiveUA();
+						inputLine = inputStream[clientCount].readUTF(); 
+						String response = inputLine.substring(16, 24);
 
 						if(response.equals("11000110") || response.equals("11001110")) {
 							System.out.println("Received UA from station " + clientID[clientCount]);
 						}
 						else {
 							System.out.println("UA error -- station " + clientID[clientCount]);
-						}       
+						}  
 
 						// initialize ns and nr
 						ns[clientCount] = -1;
@@ -131,19 +143,19 @@ class PhysicalLayerServer
 					}
 				}
 				catch (Exception e) {
-
+					e.printStackTrace();
 				}
 
 				for (i=0;i<clientCount;i++) {
 
 					// ==============================================================
-					sendRR();// insert codes here to send “RR,*,P” msg
-					System.out.println("Sent < RR,*,P > to station " + clientID[i]);
+					// insert codes here to send RR,*,P msg
+					System.out.println("Sent < RR,*,P > to station" + clientID[i]);
+					outputStream[i].writeUTF(flag + "00000001" + "10001" + "000" + flag);
 					// ==============================================================
-
-
+					
 					// recv response from the client
-					inputLine = bufferedReader[i].readLine();
+					inputLine = inputStream[i].readUTF();
 
 					if(inputLine != null) {		
 
@@ -151,21 +163,27 @@ class PhysicalLayerServer
 						response = inputLine.substring(16, 24);
 
 						if(response.substring(0,4).equals("1000")) {
-							// recv “RR,*,F”, no data to send from B
+							// recv RR,*,F, no data to send from B
 							System.out.println("Receive RR, *, F from station " + clientID[i]);
 						}
 						else if(response.substring(0, 1).equals("0")) {
 							// ==============================================================
-							handleFrame();// insert codes here to handle the frame “I, *, *” received
+							handleFrame();// insert codes here to handle the frame I, *, * received
 
 
 							//Check destination: 					
-							if(checkIfToPrimary())//if the frame is to the primary station; consume it 
+							if(checkIfToPrimary(inputLine))//if the frame is to the primary station; consume it 
 							{
-							//TODO: PROCESS THE FRAME
+								//TODO: PROCESS THE FRAME
+								//prints the information part of the frame
+								System.out.println(inputLine.substring(24, inputLine.length()-24).toString()); 
 							}
 							else{//if the frame is to the secondary station; buffer the frame to send
-							//TODO:Send to appropriate secondary station:
+								if(address[clientCount].equals(inputLine.substring(8, 15)))
+								{
+									outputStream[clientCount].writeUTF(inputLine);
+								}
+								
 							}
 
 
@@ -175,9 +193,14 @@ class PhysicalLayerServer
 				}
 
 				// ==============================================================
-				// insert codes here to send frames in the buffer       	
+				// insert codes here to send frames in the buffer   
+				outputStream[clientCount].writeUTF(inputLine);
 
 				// send I frame
+				if(control.substring( 0, 1).equals("0"))
+				{
+					outputStream[clientCount].writeUTF(inputLine);
+				}
 
 				// ==============================================================
 
@@ -194,20 +217,44 @@ class PhysicalLayerServer
 		}
 
 	}
+	
+	private static void receiveUA() {
+		String inputLine = null;
+		try {
+			inputLine = inputStream[clientCount].readUTF();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String response = inputLine.substring(16, 24);
 
+		if(response.equals("11000110") || response.equals("11001110")) {
+			System.out.println("Received UA from station " + clientID[clientCount]);
+		}
+		else {
+			System.out.println("UA error -- station " + clientID[clientCount]);
+		}  
+	}
+	
+	//handles I frames
 	private static void handleFrame() {
-		// TODO Auto-generated method stub
-		
+		int nr = 0; 
+		String data = inputLine.substring(24, inputLine.length()-8);
+		System.out.println("");
+		System.out.println("Received data: " + data);						
+
+		nr = Integer.parseInt(control.substring(1,4), 2) + 1;
+		System.out.println("nr: " + nr);
+
 	}
 
-	private static boolean checkIfToPrimary() {
-		// TODO Auto-generated method stub
+	private static boolean checkIfToPrimary(String inputLine) {
+		if(inputLine.substring(8, 15).equalsIgnoreCase("00000000"))
+		{
+			return true;
+		}
 		return false;
 	}
 
-	private static void sendRR() {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
--- src/SecondaryStation.java
+++ src/SecondaryStation.java
@@ -1,44 +1,37 @@
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.net.Socket;
-import java.net.UnknownHostException;
+import java.net.*;
+import java.io.*;
 
-class SecondaryStation 
-{
-	private static String host = "localhost";
-	private static int port = 4444;
-
-	public static void main(String args[])
-	{
+public class SecondaryStation {
+	
+	public static void main(String[] args) {
+//		 declaration section:
+//		 os: output stream
+//		 is: input stream
 		Socket clientSocket = null;
-		//PrintStream printWriter = null;
-		DataInputStream inputStream = null;
-		DataOutputStream outputStream = null;
-
-		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
+		DataOutputStream os = null;
+		DataInputStream is = null;
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
 		String id = null;
-		String information = null;
-		String control = null;
 		String flag = "01111110";
 		String address = null;  
 		String response = null; // control field of the socket input
-		int ns = -1; // send sequence number
 		int nr = 0; //receive sequence number
-
+		
+		//
 		String answer = null; // input using keyboard
-
-		//Initialization:
-		//Try to open a socket on port 4444
-		//Try to open input and output streams
+		//
+		
+       
+        //		
+//		 Initialization section:
+//		 Try to open a socket on port 4444
+//		 Try to open input and output streams
 		try {
-			clientSocket = new Socket(host, port);
-			inputStream = new DataInputStream(clientSocket.getInputStream());
-			outputStream = new DataOutputStream(clientSocket.getOutputStream());
+			clientSocket = new Socket("localhost", 2732);
+			os =  new DataOutputStream(clientSocket.getOutputStream());
+			is = new DataInputStream(clientSocket.getInputStream());
 		} 
 		catch (UnknownHostException e) {
 			System.err.println("Don't know about host: hostname");
@@ -46,68 +39,83 @@ class SecondaryStation
 		catch (IOException e) {
 			System.err.println("Couldn't get I/O for the connection to: hostname");
 		}
-
-		if (clientSocket != null && outputStream != null && inputStream != null) {			
-
+				
+		if (clientSocket != null && os != null && is != null) {			
+			
 			try {				
-
+					
 				String responseLine;				
-				responseLine = inputStream.readUTF();
-
+				responseLine = is.readUTF();
+				
 				//receive client address from the primary station
+				//
 				id = responseLine;
 				System.out.println("client address: " + id);
-
-				responseLine = inputStream.readUTF();
-				//response = responseLine.substring(16, 24);//Control Section
 				
-				String msg = "";
+				responseLine = is.readUTF();
+				response = responseLine.substring(16, 24);
+
+				// recv SNRM msg
+				if(response.equals("11000001") || response.equals("11001001")) {
+					//===========================================================
+					// insert codes here to send the UA msg		
+					System.out.println("SNRM received");
+					 String controlUA = "11000110";
+					String msg = flag + "00000000" + controlUA + "" + flag; // return message is start + primaryAddress + UA control code + (Info = 0) + end
+					os.writeUTF(msg);
+					//===========================================================
+					System.out.println("sent UA msg");
+				}
 				
-				//receive SNRM Message TODO check whether SNRM codes are correct
-				checkIfResetMsg(responseLine, outputStream, msg);// insert codes here to send the UA msg	
-
-
 				// main loop; recv and send data msgs
 				while (true) {
-					responseLine = inputStream.readUTF();
+					responseLine = is.readUTF();
 					response = responseLine.substring(16, 24);
-
+					
 					System.out.println("recv msg -- control " + response);				
-
+					
 					// recv ??RR,*,P?? msg
-					if(response.substring(0,5).equals("10001")) {
-
+					if(response.substring(0,5).equals("10001")) {//RNR
+						
 						// enter data msg using keyboard 
 						System.out.println("Is there any message to send? (y/n)");
-						answer = bufferedReader.readLine();						
-
+						answer = in.readLine();						
+						
 						if(answer.toLowerCase().equals("y") || answer.toLowerCase().equals("yes")) {
 							System.out.println("Please enter the destination address using 8-bits binary string (e.g. 00000001):");
-							address = bufferedReader.readLine();
-
+							address = in.readLine();
+							
 							System.out.println("Please enter the message to send?");
-							answer = bufferedReader.readLine();
-
+							answer = in.readLine();
+							
 							//===========================================================
 							// insert codes here to send an I msg;
-							sendIMsg();
+							
+							String msg = flag  + address + "00000000" + answer + flag;
+							os.writeUTF(msg);
+							
 							//===========================================================
-
+						
 						}				
 						else {
 							//===========================================================
 							// insert codes here to send ??RR,*,F??
-
+							System.out.println("Sending RR.");
+							String controlRR = "10000";
+							String msg = flag + address + controlRR + "000" + "" + flag; //TODO
+							
+								os.writeUTF(msg);
+							
 							//===========================================================
 						}
 					}
-
-					// receive an I frame
+					
+					// recv an I frame
 					if(response.substring(0,1).equals("0")) {
 						String data = responseLine.substring(24, responseLine.length()-8);
 						System.out.println("");
 						System.out.println("Received data: " + data);						
-
+						
 						nr = Integer.parseInt(response.substring(1,4), 2) + 1;
 						System.out.println("nr: " + nr);
 					}
@@ -117,48 +125,9 @@ class SecondaryStation
 				System.err.println("Trying to connect to unknown host: " + e);
 			} 
 			catch (IOException e) {
-				System.err.println("IOException: " + e);
+ 				System.err.println("IOException: " + e);
 			}	
 		}
-
 	}
 
-	private static void sendIMsg() {
-		// TODO Auto-generated method stub
-
-	}
-
-	//Checks whether the incoming message is reset code and requires 
-	private static void checkIfResetMsg(String response, DataOutputStream outputStream, String message) {
-
-		try {
-			// recv SNRM msg
-			if(response.equals("11000001") || response.equals("11001001")) //RESET codes
-			{
-				String msg ="";
-				//===========================================================
-				
-				outputStream.writeUTF("");//TODO
-
-				//===========================================================
-				System.out.println("sent UA msg");
-
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
-	
-	//Checks whether the currently received frame is to be rejected or processed (introduces error) 
-	//Returns true if the current frame should be accepted and processed, returns false if the current frame should be ignored.
-	boolean accept()
-	{
-		
-		
-	return true;//change	
-	}
-	
-	
-}
+}// end of class SecondaryStation
